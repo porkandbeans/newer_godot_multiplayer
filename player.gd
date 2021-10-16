cc1207a60217deb278eefc48337b81d60ce85141
hey <hey@gmail.com>
2021-10-16 03:19:39 +0100

    Hey

--- player.gd
+++ player.gd
@@ -1,25 +1,38 @@
 extends KinematicBody
 
-var speed = 5
+export var max_speed = 12
+export var gravity = 70
+export var jump_impulse = 25
 
-var direction = Vector3()
+var velocity = Vector3.ZERO
 
-func _ready():
-	pass
+func _physics_process(delta):
+	var input_vector = get_input_vector()
+	apply_movement(input_vector)
+	apply_gravity(delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+
+func get_input_vector():
+	var input_vector = Vector3.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	return input_vector.normalized()
+
+func apply_movement(input_vector):
+	velocity.x = input_vector.x * max_speed
+	velocity.z = input_vector.z * max_speed
+	
+	
+func apply_gravity(delta):
+	velocity.y -= gravity * delta
 
 remote func _set_position(pos):
 	global_transform.origin = pos
 
-func _physics_process(_delta):
-	direction = Vector3()
 	
-	if Input.is_action_pressed("ui_left"):
-		direction -= transform.basis.x
-	elif Input.is_action_pressed("ui_right"):
-		direction += transform.basis.x
-	direction = direction.normalized()
-	
-	if direction != Vector3():
+	if velocity != Vector3.ZERO:
 		if is_network_master():
-			move_and_slide(direction * speed, Vector3.UP)
+			move_and_slide(velocity * max_speed, Vector3.UP)
 			rpc_unreliable("_set_position", global_transform.origin)
