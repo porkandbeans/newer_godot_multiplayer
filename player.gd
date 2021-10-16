6e829994c9ce5df55b59ea5b65f6cd2581cf1323
hey <hey@gmail.com>
2021-10-16 03:36:41 +0100

    g

--- player.gd
+++ player.gd
@@ -4,13 +4,13 @@ export var max_speed = 12
 export var gravity = 70
 export var jump_impulse = 25
 
-var velocity = Vector3.ZERO
+var direction = Vector3.ZERO
 
 func _physics_process(delta):
 	var input_vector = get_input_vector()
 	apply_movement(input_vector)
 	apply_gravity(delta)
-	velocity = move_and_slide(velocity, Vector3.UP)
+	direction = move_and_slide(direction, Vector3.UP)
 
 
 func get_input_vector():
@@ -21,18 +21,18 @@ func get_input_vector():
 	return input_vector.normalized()
 
 func apply_movement(input_vector):
-	velocity.x = input_vector.x * max_speed
-	velocity.z = input_vector.z * max_speed
+	direction.x = input_vector.x * max_speed
+	direction.z = input_vector.z * max_speed
 	
 	
 func apply_gravity(delta):
-	velocity.y -= gravity * delta
+	direction.y -= gravity * delta
 
 remote func _set_position(pos):
 	global_transform.origin = pos
 
 	
-	if velocity != Vector3():
+	if direction != Vector3():
 		if is_network_master():
-			move_and_slide(velocity * max_speed, Vector3.UP)
+			move_and_slide(direction * max_speed, Vector3.UP)
 			rpc_unreliable("_set_position", global_transform.origin)
