67d520ff1d82feee89837697870701dca3331e5f
hey <hey@gmail.com>
2021-10-25 08:19:04 +0100

    2

--- Game.gd
+++ Game.gd
@@ -5,13 +5,13 @@ onready var player2Pos = $player2Pos
 
 func _ready():
 	var player1 = preload("res://player.tscn").instance()
-	player1.set_name(str(get_tree().get_network_unique_id()))
+	player1.set_name("player_" + str(get_tree().get_network_unique_id()))
 	player1.set_network_master(get_tree().get_network_unique_id())
 	player1.global_transform = player1Pos.global_transform
 	add_child(player1)
 	
 	var player2 = preload("res://player.tscn").instance()
-	player2.set_name(str(Globals.player2id))
+	player2.set_name("player_" + str(Globals.player2id))
 	player2.set_network_master(Globals.player2id)
 	player2.global_transform = player2Pos.global_transform
 	add_child(player2)
--- soloGame.gd
+++ soloGame.gd
@@ -4,6 +4,6 @@ onready var player1Pos = $player1Pos
 
 func _ready():
 	var player1 = preload("res://player.tscn").instance()
-	player1.set_name("cunt")
+	player1.set_name("player")
 	player1.global_transform = player1Pos.global_transform
 	add_child(player1)
